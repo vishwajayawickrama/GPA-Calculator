19fa6c571572e9e9c2ebb51cf49053b4635cce8f
vishwajayawickrama <pmvishwajayawickrama@gmail.com>
2024-02-10 03:25:36 +0530

    Button to add new Subject with their respective grades

---
+++ src/components/Subjects.js
@@ -0,0 +1,9 @@
+export const Subjects = () => {
+    return (
+        <>
+            <form>
+                <button type="submit">+ Add New Subject</button>
+            </form>
+        </>
+    );
+}
