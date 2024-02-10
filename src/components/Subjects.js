954b0ad9d1321f4a6c25ed68b21e97b90ca9dbc2
vishwajayawickrama <pmvishwajayawickrama@gmail.com>
2024-02-11 00:06:06 +0530

    Some Code testing

--- src/components/Subjects.js
+++ src/components/Subjects.js
@@ -1,44 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 export const Subjects = () => {
+  const [formData, setFormData] = useState({
+    modulecode: "Module Code",
+    modulename: "",
+    credits: "",
+    grade: "",
+  });
 
-    const [formData, setFormData] = useState({
-        modulecode: '',
-        modulename: '',
-        credits: '',
-        grade: ''
-      });
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({
-          ...formData,
-          [name]: value
-        });
-      };
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    }
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form submitted with data:", formData);
+  };
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="modulecode" value="Module Code" onChange={handleInputChange}></input>
-        <input type="text" name="modulename" value="Module Name" onChange={handleInputChange}></input>
-        <input type="text" name="credits" value="Credits Offered" onChange={handleInputChange}></input>
+        <input
+          type="text"
+          name="modulecode"
+          value={formData.modulecode}
+          onChange={handleInputChange}
+        ></input>
+        <input
+          type="text"
+          name="modulename"
+          value="Module Name"
+          onChange={handleInputChange}
+        ></input>
+        <input
+          type="text"
+          name="credits"
+          value="Credits Offered"
+          onChange={handleInputChange}
+        ></input>
         <select id="grade" name="grade" onChange={handleInputChange}>
-            <option value="" disabled selected>Grade</option>
-            <option value="A+">A+</option>
-            <option value="A">A</option>
-            <option value="A-">A-</option>
-            <option value="B+">B+</option>
-            <option value="B">B</option>
-            <option value="B-">B-</option>
-            <option value="C+">C+</option>
-            <option value="C">C</option>
-            <option value="C-">C-</option>
-            <option value="D">D</option>
-            <option value="I">I</option>
+          <option value="" disabled selected>
+            Grade
+          </option>
+          <option value="A+">A+</option>
+          <option value="A">A</option>
+          <option value="A-">A-</option>
+          <option value="B+">B+</option>
+          <option value="B">B</option>
+          <option value="B-">B-</option>
+          <option value="C+">C+</option>
+          <option value="C">C</option>
+          <option value="C-">C-</option>
+          <option value="D">D</option>
+          <option value="I">I</option>
         </select>
-        <br/>
+        <br />
         <button type="submit">+ Add New Subject</button>
       </form>
     </>
