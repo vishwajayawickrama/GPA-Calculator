f9800467aeac4bc002ff710fd27244e78bef2576
vishwajayawickrama <pmvishwajayawickrama@gmail.com>
2024-02-11 15:52:36 +0530

    Array to store subject details and table to show that details

--- src/components/Subjects.js
+++ src/components/Subjects.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 export const Subjects = () => {
+  const [formsData, setFormsData] = useState([]);   // Array to store all subjects
   const [formData, setFormData] = useState({
     modulecode: "Module Code",
-    modulename: "",
-    credits: "",
+    modulename: "Module Name",
+    credits: "Credits Offered",
     grade: "",
   });
 
   const handleInputChange = (event) => {
+    // When input changes take previos values from object and add new data to it
     const { name, value } = event.target;
     setFormData({
       ...formData,
@@ -15,8 +17,15 @@ export const Subjects = () => {
     });
   };
   const handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault(); // Prevent default submitting form
     console.log("Form submitted with data:", formData);
+    setFormsData([...formsData, formData]); //Adding new object to array
+    setFormData({     // Clearing out previous values from form object
+      modulecode: "Module Code",
+      modulename: "Module Name",
+      credits: "Credits Offered",
+      grade: formData.grade,
+    });
   };
   return (
     <>
@@ -34,18 +43,23 @@ export const Subjects = () => {
         <input
           type="text"
           name="modulename"
-          value="Module Name"
+          value={formData.modulename}
           onChange={handleInputChange}
           className="m-2"
         ></input>
         <input
           type="text"
           name="credits"
-          value="Credits Offered"
+          value={formData.credits}
           onChange={handleInputChange}
           className="m-2"
         ></input>
-        <select id="grade" name="grade" onChange={handleInputChange} className="m-2">
+        <select
+          id="grade"
+          name="grade"
+          onChange={handleInputChange}
+          className="m-2"
+        >
           <option value="" disabled selected>
             Grade
           </option>
@@ -64,6 +78,28 @@ export const Subjects = () => {
         <br />
         <button type="submit">+ Add New Subject</button>
       </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Module Code</th>
+            <th>Module Name</th>
+            <th>Credits Offered</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {formsData.map((data, index) => (
+            <tr key={index}>             
+              {/* TODO: Add delete row button to delete entire subject */}
+              <td>{data.modulecode}</td>
+              <td>{data.modulename}</td>
+              <td>{data.credits}</td>
+              <td>{data.grade}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
